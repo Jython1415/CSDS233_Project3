3957fbdfccd0d0e54bc5e13ef142816c57f4dc84
Joshua Shew <joshua.t.shew@gmail.com>
2022-02-28 20:18:27 -0500

    added method headers

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,25 @@
-public class BinarySearchTree {
+import java.lang.Comparable;
+import java.util.List;
+
+public class BinarySearchTree<T extends Comparable<? super T>, V> {
     
+    public void insert(T key, V value) {
+
+    }
+
+    public V search(T key) {
+        return null;
+    }
+
+    public void delete(T key) {
+        
+    }
+
+    public List<V> inorderRec() {
+        return null;
+    }
+
+    public V kthSmallest(int k) {
+        return null;
+    }
 }
