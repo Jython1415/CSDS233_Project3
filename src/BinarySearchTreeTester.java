4baa533550b0e936a7f898f5faf19cbe1e9392d0
Joshua Shew <joshua.t.shew@gmail.com>
2022-03-02 22:13:48 -0500

    Added testing class

---
+++ src/BinarySearchTreeTester.java
@@ -0,0 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tester class for BinarySearchTree
+ * @author Joshua Shew
+ */
+public class BinarySearchTreeTester {
+    /**
+     * Unit tests for insert method
+     */
+    public void testInsert() {
+
+    }
+
+    /**
+     * Unit tests for search method
+     */
+    public void testSearch() {
+
+    }
+
+    /**
+     * Unit tests for delete method
+     */
+    public void testDelete() {
+
+    }
+
+    /**
+     * Unit tests for inorderRec method
+     */
+    public void testInorderRec() {
+
+    }
+
+    /**
+     * Unit tests for kthSmallest method
+     */
+    public void kthSmallest() {
+
+    }
+}
